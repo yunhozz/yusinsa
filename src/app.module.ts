d510cb94bc0a949ab78072310fe072d7b4f274d2
yunhozz <qkrdbsgh1121@naver.com>
2023-03-12 17:37:58 +0900

    feat: users 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import {Module} from '@nestjs/common';
+import {UsersModule} from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule]
 })
 export class AppModule {}
