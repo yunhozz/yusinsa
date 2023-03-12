ba379574c4fb9cadec376363ac9e82238b062372
yunhozz <qkrdbsgh1121@naver.com>
2023-03-12 19:00:59 +0900

    chore: 커스텀 repository 모듈 등록

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,11 @@ import {UsersController} from './users.controller';
 import {UsersService} from './users.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {User} from "./domain/user.entity";
+import {TypeOrmCustomModule} from "../config/type-orm-custom.module";
+import {UserRepository} from "./user.repository";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), TypeOrmCustomModule.forCustomRepository([UserRepository])],
   controllers: [UsersController],
   providers: [UsersService]
 })
