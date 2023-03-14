f9b5fb5f67b3630e65e5582c10349e5cffbb3e2a
yunhozz <qkrdbsgh1121@naver.com>
2023-03-14 19:38:15 +0900

    fix: validation 수정

--- src/users/dto/user-request.dto.ts
+++ src/users/dto/user-request.dto.ts
@@ -1,14 +1,4 @@
-import {
-    IsEmail,
-    IsInt,
-    IsNotEmpty,
-    IsPhoneNumber,
-    IsString,
-    IsStrongPassword,
-    Length,
-    Matches,
-    MaxLength
-} from "class-validator";
+import {IsEmail, IsInt, IsNotEmpty, IsNumberString, IsString, Length, Matches, MaxLength} from "class-validator";
 import {PartialType, PickType} from "@nestjs/mapped-types";
 import {Gender} from "../domain/gender.enum";
 
@@ -17,7 +7,7 @@ export class CreateUserRequestDto {
     @MaxLength(30, { message: '글자 수 30 이하로 입력해주세요.' })
     email: string;
 
-    @IsStrongPassword()
+    @IsString()
     @Length(6, 12, { message: '6 ~ 12글자 사이로 입력해주세요.' })
     @Matches(/^[a-zA-Z0-9]*$/, { message: '패스워드는 영어와 숫자로만 구성할 수 있습니다.' })
     password: string;
@@ -43,7 +33,7 @@ export class CreateUserRequestDto {
     @IsString()
     etc: string;
 
-    @IsPhoneNumber()
+    @IsNumberString()
     phoneNumber: number;
 }
 
