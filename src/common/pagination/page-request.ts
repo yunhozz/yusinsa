5a4e94f5736a805df6cc53c5d01e105e4f98f1e3
yunhozz <qkrdbsgh1121@naver.com>
2023-05-05 16:19:46 +0900

    feat: pagination 에 관한 클래스 추가

---
+++ src/common/pagination/page-request.ts
@@ -0,0 +1,32 @@
+import {IsOptional, IsString} from "class-validator";
+
+export class PageRequest {
+
+    @IsString()
+    @IsOptional() // undefined 도 받을 수 있다.
+    pageNo?: number | 1;
+
+    @IsString()
+    @IsOptional()
+    pageSize?: number | 10;
+
+    getOffset(): number {
+        if (this.pageNo < 1 || this.pageNo === null || this.pageNo === undefined) {
+            this.pageNo = 1;
+        }
+
+        if (this.pageSize < 1 || this.pageSize === null || this.pageSize === undefined) {
+            this.pageSize = 10;
+        }
+
+        return (Number(this.pageNo - 1)) * Number(this.pageSize);
+    }
+
+    getLimit(): number {
+        if (this.pageSize < 1 || this.pageSize === null || this.pageSize === undefined) {
+            this.pageSize = 10;
+        }
+
+        return Number(this.pageSize);
+    }
+}
