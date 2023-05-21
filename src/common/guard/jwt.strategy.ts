12c073de72dcb085466b120a1d8371cc6e46438e
yunhozz <qkrdbsgh1121@naver.com>
2023-05-21 18:18:00 +0900

    feat: 쿠키에서 jwt 토큰 호출에 대해 로깅 추가

--- src/common/guard/jwt.strategy.ts
+++ src/common/guard/jwt.strategy.ts
@@ -1,5 +1,5 @@
 import {PassportStrategy} from "@nestjs/passport";
-import {Injectable} from "@nestjs/common";
+import {Injectable, Logger} from "@nestjs/common";
 import {UserRepository} from "../../users/user.repository";
 import {ExtractJwt, Strategy} from "passport-jwt";
 import {TokenPayload} from "../../users/dto/token.payload";
@@ -12,15 +12,23 @@ const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+
+    private readonly logger: Logger = new Logger(JwtStrategy.name);
+
     constructor(private readonly userRepository: UserRepository) {
         super({
             secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
-            jwtFromRequest: ExtractJwt.fromExtractors([(req: Request) => req?.cookies?.token])
+            jwtFromRequest: ExtractJwt.fromExtractors([(req: Request) => {
+                const token = req?.cookies?.token;
+                this.logger.log(`Access Token: ${token}`);
+                return token;
+            }])
         });
     }
 
     async validate(payload: TokenPayload): Promise<User> {
         const email = payload.email;
+        this.logger.log(`Validated User: ${email}`);
         return await this.userRepository.findOneBy({ email });
     }
 }
