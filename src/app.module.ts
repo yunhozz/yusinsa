431ee4bc2e93089771507e432b33590046f08a3e
yunhozz <qkrdbsgh1121@naver.com>
2023-05-28 16:31:34 +0900

    chore: redis 관련 의존성 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,17 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {typeOrmConfig} from "./config/type-orm.config";
 import {APP_GUARD} from "@nestjs/core";
 import {RolesGuard} from "./common/guard/roles.guard";
-import {CacheModule} from "@nestjs/cache-manager";
+import {RedisModule} from "@liaoliaots/nestjs-redis";
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(typeOrmConfig),
-        CacheModule.register({
-            isGlobal: true,
-            host: 'localhost',
-            port: 6379,
-            ttl: 0
+        RedisModule.forRoot({
+           closeClient: true,
+           config: {
+               host: '127.0.0.1',
+               port: 6379
+           }
         }),
         UsersModule
     ],
