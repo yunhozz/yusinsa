52ad98986abd51a1debc71edc3348e59385d1078
yunhozz <qkrdbsgh1121@naver.com>
2023-06-06 17:29:00 +0900

    fix: timestamp 표시 방식 수정

--- src/common/response/failure.ts
+++ src/common/response/failure.ts
@@ -2,10 +2,10 @@ import {Result} from "./result";
 
 export class Failure implements Result {
 
-    timestamp: number;
+    timestamp: Date;
     errMsg: string;
 
-    constructor(timestamp: number, errMsg: string) {
+    constructor(timestamp: Date, errMsg: string) {
         this.timestamp = timestamp;
         this.errMsg = errMsg;
     }
