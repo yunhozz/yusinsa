526930c9d621e6bd312e0d275279707983d79ffc
yunhozz <qkrdbsgh1121@naver.com>
2023-06-06 17:32:05 +0900

    feat: redis 데이터 조회, 삭제 로직 추가

--- src/users/redis-custom.service.ts
+++ src/users/redis-custom.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, NotFoundException} from "@nestjs/common";
 import {RedisService} from "@liaoliaots/nestjs-redis";
 import {Redis} from "ioredis";
 
@@ -11,10 +11,20 @@ export class RedisCustomService {
     private readonly redis: Redis;
 
     async get(key: string): Promise<string> {
-        return await this.redis.get(key);
+        const value = await this.redis.get(key);
+        if (!value) {
+            throw new NotFoundException(`redis 에 해당 key 에 대한 데이터가 없습니다 : ${key}`);
+        }
+
+        return value;
     }
 
     async set(key: string, value: string, ttl?: number): Promise<void> {
         await this.redis.set(key, value, 'EX', ttl);
+    }
+
+    async delete(key: string): Promise<void> {
+        const value = await this.get(key);
+        await this.redis.set(key, value, 'PX', 1);
     }
 }
