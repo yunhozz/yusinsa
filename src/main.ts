5ff35f5bb5a0b5b29a6ae1501d43225b450890b3
yunhozz <qkrdbsgh1121@naver.com>
2023-06-06 17:31:33 +0900

    chore: cors 관련 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import {AppModule} from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin : ['http://localhost'],
+    credentials : true
+  });
   app.use(cookieParser());
   await app.listen(3000);
 }
