cfd2bb0a073d1ae7119d71aa3fc8b4f9916f8d01
yunhozz <qkrdbsgh1121@naver.com>
2023-06-11 16:21:41 +0900

    fix: 리턴값 명시

--- src/common/decorator/roles.decorator.ts
+++ src/common/decorator/roles.decorator.ts
@@ -3,4 +3,4 @@ import {Role} from "../../users/user.entity";
 
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: Role[]): MethodDecorator => SetMetadata(ROLES_KEY, roles);
