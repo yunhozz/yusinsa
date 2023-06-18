c18261843a26eb7b3a62eb58a0e83b3bd0b73045
yunhozz <qkrdbsgh1121@naver.com>
2023-06-18 15:40:14 +0900

    style: 공백 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,17 @@ import {typeOrmConfig} from "./config/type-orm.config";
 import {APP_GUARD} from "@nestjs/core";
 import {RolesGuard} from "./common/decorator/roles.guard";
 import {RedisModule} from "@liaoliaots/nestjs-redis";
-import { OrdersModule } from './orders/orders.module';
+import {OrdersModule} from './orders/orders.module';
 
 @Module({
     imports : [
         TypeOrmModule.forRoot(typeOrmConfig),
         RedisModule.forRoot({
-           closeClient : true,
-           config : {
+            closeClient : true,
+            config : {
                host : '127.0.0.1',
                port : 6379
-           }
+            }
         }),
         UsersModule,
         OrdersModule
