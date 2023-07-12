29066f5d261de2357d0d7f6bc531095743ad0bd8
yunhozz <qkrdbsgh1121@naver.com>
2023-07-12 20:40:50 +0900

    style: import 문 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import {Module} from '@nestjs/common';
-import {UsersModule} from './users/users.module';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {typeOrmConfig} from "./config/type-orm.config";
-import {APP_GUARD} from "@nestjs/core";
-import {RolesGuard} from "./common/decorator/roles.guard";
-import {RedisModule} from "@liaoliaots/nestjs-redis";
-import {OrdersModule} from './orders/orders.module';
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/type-orm.config';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './common/decorator/roles.guard';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
     imports : [
