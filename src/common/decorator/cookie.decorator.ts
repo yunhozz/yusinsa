ba56448f9fa11835a679b51e0a699cc86db73915
yunhozz <qkrdbsgh1121@naver.com>
2023-07-14 08:47:49 +0900

    style: import 문 수정

--- src/common/decorator/cookie.decorator.ts
+++ src/common/decorator/cookie.decorator.ts
@@ -1,4 +1,4 @@
-import {createParamDecorator, ExecutionContext} from "@nestjs/common";
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const Cookie = createParamDecorator((data: string, context: ExecutionContext) => {
     const req = context.switchToHttp().getRequest();
