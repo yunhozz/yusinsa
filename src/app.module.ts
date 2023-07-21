8316d0fa254f1fa53b0613f2fc9757ad3160b3a3
yunhozz <qkrdbsgh1121@naver.com>
2023-07-21 20:51:49 +0900

    fix: AppModule에 filter, interceptor 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,29 @@
-import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm/type-orm.config';
 import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpExceptionFilter } from './config/exception/http-exception.filter';
+import { PipeInterceptor } from './config/exception/pipe.interceptor';
+import { TypeOrmConfig } from './config/typeorm/type-orm.config';
 import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports : [
-        TypeOrmModule.forRoot(typeOrmConfig),
+    imports: [
+        TypeOrmModule.forRoot(TypeOrmConfig),
         RedisModule.forRoot({
-            closeClient : true,
-            config : {
-               host : '127.0.0.1',
-               port : 6379
+            closeClient: true,
+            config: {
+                host: '127.0.0.1',
+                port: 6379
             }
         }),
         UsersModule,
         OrdersModule
+    ],
+    providers: [
+        { provide: APP_FILTER, useClass: HttpExceptionFilter },
+        { provide: APP_INTERCEPTOR, useClass: PipeInterceptor }
     ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
-import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './config/exception/http-exception.filter';
-import { ValidationExceptionFilter } from './config/exception/validation-exception.filter';
-import { PipeInterceptor } from './config/exception/pipe.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin : ['http://localhost'],
-        credentials : true,
+        origin: ['http://localhost'],
+        credentials: true,
     });
     app.use(cookieParser());
-    app.useGlobalFilters(new HttpExceptionFilter(), new ValidationExceptionFilter())
-    app.useGlobalInterceptors(new PipeInterceptor())
     await app.listen(3000);
 }
 bootstrap();
